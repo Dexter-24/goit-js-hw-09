1c34bdca95c6274fce92ca028b8e393a5c8f0db5
Dexter-24 <67460733+Dexter-24@users.noreply.github.com>
2024-06-20 13:31:41 +0000

    Deploying to gh-pages from @ Dexter-24/goit-js-hw-09@3937d0c74441e99cb309d74f7a9e30ef958327bb 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";const e={email:"",message:""},a=document.querySelector(".feedback-form"),n=a.elements.email,r=a.elements.message,s="feedback-form-state";document.addEventListener("DOMContentLoaded",()=>{const t=localStorage.getItem(s);if(t){const o=JSON.parse(t);n.value=o.email,r.value=o.message,e.email=o.email,e.message=o.message}});a.addEventListener("input",t=>{e[t.target.name]=t.target.value,localStorage.setItem(s,JSON.stringify(e))});a.addEventListener("submit",t=>{if(t.preventDefault(),!e.email.trim||!e.message){alert("All fields should be filled");return}console.log("Form Data",e),localStorage.removeItem(s),e.email="",e.message="",a.reset()});const l=document.createElement("style");l.textContent=`
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e={email:"",message:""},a=document.querySelector(".feedback-form"),n=a.elements.email,r=a.elements.message,s="feedback-form-state";document.addEventListener("DOMContentLoaded",()=>{const t=localStorage.getItem(s);if(t){const o=JSON.parse(t);n.value=o.email,r.value=o.message,e.email=o.email,e.message=o.message}});a.addEventListener("input",t=>{e[t.target.name]=t.target.value,localStorage.setItem(s,JSON.stringify(e))});a.addEventListener("submit",t=>{if(t.preventDefault(),!e.email.trim()||!e.message.trim()){alert("All fields should be filled");return}console.log("Form Data",e),localStorage.removeItem(s),e.email="",e.message="",a.reset()});const l=document.createElement("style");l.textContent=`
             .feedback-form {
                 display: flex;
                 flex-direction: column;
@@ -7,12 +7,12 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";const e={email:"",message:""
                 width: 408px;
             }
             label {
-            font-family: "Montserrat", sans-serif;
-font-weight: 400;
-font-size: 16px;
-line-height: 1.5;
-letter-spacing: 0.04em;
-color: #2e2f42;
+                font-family: "Montserrat", sans-serif;
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 1.5;
+                letter-spacing: 0.04em;
+                color: #2e2f42;
                 display: flex;
                 flex-direction: column;
             }
